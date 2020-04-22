c1e2865f6c0b4dc5090704a3cd01f55aa4e151e1
Ryan State <rwstate@gmail.com>
2020-04-22 10:06:20 -0400

    finish day 2 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,6 +5,7 @@ import sys
 HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
+MUL = 0b10100010
 
 class CPU:
     """Main CPU class."""
@@ -28,19 +29,18 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        with open(sys.argv[1]) as f:
+            for line in f:
+              line = line.split('#')
+              line = line[0].strip()
+          
+              if line == '':
+                continue
+          
+              self.memory[address] = int(line, 2)
+          
+              address += 1
 
-        for instruction in program:
-            self.memory[address] = instruction
-            address += 1
 
 
     # def alu(self, op, reg_a, reg_b):
@@ -86,4 +86,7 @@ class CPU:
             elif ir == PRN:
                 print(self.r[operand_a])
                 self.pc += 2
+            elif ir == MUL:
+                self.r[operand_a] = self.r[operand_a] * self.r[operand_b]
+                self.pc += 3
             
